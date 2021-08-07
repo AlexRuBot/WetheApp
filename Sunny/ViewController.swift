ddcb979aa28c68fb74a03cfe89ee2fbe5e124fe3
Саша Гужавин <sashaguzavin@gmail.com>
2021-08-07 15:46:30 +0300

    Initial Commit

---
+++ Sunny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sunny
+//
+//  Created by Саша Гужавин on 07.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
